7c94a4a1f0ab3d3a5c7556c58e846a196006fe65
felipeghidini <lipeghidini18@gmail.com>
2021-07-29 21:17:49 -0300

    Projeto 3 concluído

--- projeto3/script.js
+++ projeto3/script.js
@@ -4,6 +4,7 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
     let input = document.querySelector('#searchInput').value;
 
     if (input !== '') {
+        clearInfo();
         showWarning('Carregando...');
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=314c244742bf6882cfc26e0b308d6c4a&units=metric&lang=pt_br`;
@@ -11,9 +12,38 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
         let results = await fetch(url);
         let json = await results.json();
 
-        console.log(json);
+        if ( json.cod === 200) {
+            showInfo({ 
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        } else {
+            clearInfo();
+            showWarning('Não encontramos esta localização.');
+        }
     }
 });
+
+function showInfo(json) {
+    showWarning('');    
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>º</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed}<span>km/h</span>`;
+    
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+    document.querySelector('ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+    
+    document.querySelector('.resultado').style.display = 'block';
+}
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none'
+}
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg; 
