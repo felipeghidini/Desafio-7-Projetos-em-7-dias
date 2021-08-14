fac2dca5841839aebde15a54819671aec4bb1399
felipeghidini <lipeghidini18@gmail.com>
2021-08-14 06:53:15 -0300

    Finalizado projeto de quadro Canvas.

--- Projeto 5/script.js
+++ Projeto 5/script.js
@@ -1,11 +1,19 @@
 // Initial data
 let currentColor = 'black';
-
+let canDraw = false;
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d');
+let mouseX = 0;
+let mouseY = 0;
 
 // Events
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen);
 
 //Functions
 function colorClickEvent(e) {
@@ -14,4 +22,46 @@ function colorClickEvent(e) {
 
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
+}
+
+function mouseDownEvent(e) {
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(e) {
+    if (canDraw) {
+        draw(e.pageX, e.pageY);
+    /* let pointX = e.pageX - screen.offsetLeft; Zerar a posição do mouse no quadro 
+       let pointY = e.pageY - screen.offsetTop; 
+       console.log(pointX, pointY);
+       console.log(e.pageX, e.pageY); Consigo ver a posição do mouse com relação à tela */
+    }
+}
+
+function mouseUpEvent() {
+    canDraw = false;
+}
+
+function draw(x, y) {
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+function clearScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
