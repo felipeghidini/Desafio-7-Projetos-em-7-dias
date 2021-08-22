e7563935b2366320b1c77f0ddda2f4eae80b8744
felipeghidini <lipeghidini18@gmail.com>
2021-08-22 09:35:00 -0300

    Ajuste na lógica de mudança de questões

--- Projeto 6/script.js
+++ Projeto 6/script.js
@@ -1,4 +1,5 @@
 let currentQuestion = 0;
+let correctAnswers = 0;
 
 showQuestion();
 
@@ -6,16 +7,44 @@ function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion];
 
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
+
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
 
         document.querySelector('.question').innerHTML = q.question;
+
         let optionsHtml = '';
         for (let i in q.options) {
-            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.questions[i]}</div>`;            
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`;            
         }
         document.querySelector('.options').innerHTML = optionsHtml;
-    } else {
 
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);            
+        });
+
+    } else {
+        finishQuis();
     }
+}
+
+function optionClickEvent(e) {
+    let clickedOption = parseInt(e.target.getAttribute('data-op'));
+
+    if (questions[currentQuestion].answer === clickedOption) {
+        correctAnswers++;
+        console.log(clickedOption);
+    }
+
+    currentQuestion++;
+    showQuestion();
+}
+
+function finishQuis() {
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress-bar').style.width = '100%';
+
 }
